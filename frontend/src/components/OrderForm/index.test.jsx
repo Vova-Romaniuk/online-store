ff59c3afd42992f91be0fde75d92069444634bea
Vova Romanyuck <vovaromanyuck@MacBook-Air-Vova.local>
2023-12-01 08:04:16 +0200

    update some changes

--- frontend/src/components/OrderForm/index.test.jsx
+++ frontend/src/components/OrderForm/index.test.jsx
@@ -1,52 +1,53 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { BrowserRouter as Router } from "react-router-dom";
 import OrderForm from ".";
 
-test('renders "Повернутися на головну" button', () => {
-	render(
-		<Router>
-			<OrderForm />
-		</Router>
-	);
+describe("OrderForm component tests", () => {
+	test('renders "Повернутися на головну" button', () => {
+		render(
+			<Router>
+				<OrderForm />
+			</Router>
+		);
 
-	const backButton = screen.getByText("Повернутися на головну");
-	expect(backButton).toBeInTheDocument();
+		const backButton = screen.getByText("Повернутися на головну");
+		expect(backButton).toBeInTheDocument();
+	});
+
+	test('renders input for "First Name"', () => {
+		render(
+			<Router>
+				<OrderForm />
+			</Router>
+		);
+
+		const firstNameInput = screen.getByLabelText("First Name:");
+		expect(firstNameInput).toBeInTheDocument();
+		expect(firstNameInput).toHaveAttribute("type", "text");
+	});
+
+	test('renders input for "Email"', () => {
+		render(
+			<Router>
+				<OrderForm />
+			</Router>
+		);
+
+		const emailInput = screen.getByLabelText("Email:");
+		expect(emailInput).toBeInTheDocument();
+		expect(emailInput).toHaveAttribute("type", "email");
+	});
+
+	test('renders input for "Phone Number"', () => {
+		render(
+			<Router>
+				<OrderForm />
+			</Router>
+		);
+
+		const phoneNumberInput = screen.getByLabelText("Phone Number:");
+		expect(phoneNumberInput).toBeInTheDocument();
+		expect(phoneNumberInput).toHaveAttribute("type", "tel");
+	});
 });
-
-test('renders input for "First Name"', () => {
-	render(
-		<Router>
-			<OrderForm />
-		</Router>
-	);
-
-	const firstNameInput = screen.getByLabelText("First Name:");
-	expect(firstNameInput).toBeInTheDocument();
-	expect(firstNameInput).toHaveAttribute("type", "text");
-});
-
-test('renders input for "Email"', () => {
-	render(
-		<Router>
-			<OrderForm />
-		</Router>
-	);
-
-	const emailInput = screen.getByLabelText("Email:");
-	expect(emailInput).toBeInTheDocument();
-	expect(emailInput).toHaveAttribute("type", "email");
-});
-
-test('renders input for "Phone Number"', () => {
-	render(
-		<Router>
-			<OrderForm />
-		</Router>
-	);
-
-	const phoneNumberInput = screen.getByLabelText("Phone Number:");
-	expect(phoneNumberInput).toBeInTheDocument();
-	expect(phoneNumberInput).toHaveAttribute("type", "tel");
-});
-
-// Додайте інші тести, які вам потрібні для перевірки форми
